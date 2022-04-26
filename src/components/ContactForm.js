7e1062a3bd87a53779521f2d238dcd7e788950e7
Michał Niewitała <niewitala@lightweb.pl>
2022-04-26 15:39:30 +0200

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -22,33 +22,14 @@ function TextInput({ type = "text", name }) {
 
 const ContactForm = ({ id }) => {
   return (
-    <form name="contact" method="POST" data-netlify="true">
-      <p>
-        <label>
-          Your Name: <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email: <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Role:{" "}
-          <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select>
-        </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message" defaultValue={""} />
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
+    <form className={styles.contactForm} name="contact" method="POST" data-netlify="true">
+      <TextInput name="name" />
+      <TextInput name="email" type="email" />
+      <TextInput name="message" type="textarea" />
+      <p className={styles.field}>
+        <button className={styles.button} type="submit">
+          Send message
+        </button>
       </p>
     </form>
   );
