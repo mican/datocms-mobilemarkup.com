550b27fe7baec326acd184c0b64e8220ac567394
Michał Niewitała <niewitala@lightweb.pl>
2022-04-26 15:32:04 +0200

    Test form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -14,7 +14,7 @@ function TextInput({ type = "text", name }) {
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
-      {type === "textarea" && <textarea className={styles.textarea} cols="50" rows="4" name={name} id={name} onChange={handleInput} required />}
+      {type === "textarea" && <textarea className={styles.textarea} cols="50" rows="4" name={name} id={name} onChange={handleInput}></textarea>}
       {type !== "textarea" && <input className={styles.textInput} type={type} name={name} id={name} onChange={handleInput} required />}
     </p>
   );
@@ -22,14 +22,33 @@ function TextInput({ type = "text", name }) {
 
 const ContactForm = ({ id }) => {
   return (
-    <form className={styles.contactForm} name="contact" method="POST" netlify>
-      <TextInput name="name" />
-      <TextInput name="email" type="email" />
-      <TextInput name="message" type="textarea" />
-      <p className={styles.field}>
-        <button className={styles.button} type="submit">
-          Send message
-        </button>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Role:{" "}
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select>
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message" defaultValue={""} />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
       </p>
     </form>
   );
