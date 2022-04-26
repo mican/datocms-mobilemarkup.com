34b0820a9cfc79905d02ed6d973791c95c8495ec
Michał Niewitała <niewitala@lightweb.pl>
2022-04-26 15:14:40 +0200

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,13 +5,17 @@ function TextInput({ type = "text", name }) {
   const [value, setValue] = useState("");
   var label = name.charAt(0).toUpperCase() + name.slice(1);
 
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <p className={styles.field + (value && " active")}>
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
-      {type === "textarea" && <textarea className={styles.textarea} cols="50" rows="4" name={name} id={name} required />}
-      {type !== "textarea" && <input className={styles.textInput} type={type} name={name} id={name} required />}
+      {type === "textarea" && <textarea className={styles.textarea} cols="50" rows="4" name={name} id={name} onChange={handleInput} required />}
+      {type !== "textarea" && <input className={styles.textInput} type={type} name={name} id={name} onChange={handleInput} required />}
     </p>
   );
 }
