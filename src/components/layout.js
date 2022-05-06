47d34c5183d75369108559428965aef9f59084ea
Michał Niewitała <niewitala@lightweb.pl>
2022-05-06 12:48:58 +0200

    ScrollSmoother

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import Header from "./header";
+
+import { gsap, ScrollTrigger, ScrollSmoother } from "gsap-trial/all";
 
 import "@fontsource/poppins/300.css";
 import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/500.css";
 
 import "normalize.css";
-import "locomotive-scroll/dist/locomotive-scroll.css";
 import "../styles/layout.sass";
 
-const Layout = ({ children, className, scrollRef }) => {
+const Layout = ({ children, className }) => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+    ScrollSmoother.create({
+      smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+      effects: true, // looks for data-speed and data-lag attributes on elements
+      smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+    });
+  });
   return (
-    <main className={className} ref={scrollRef}>
+    <main className={className}>
       <Header />
       {children}
     </main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
-import React, { useEffect, useRef } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import locomotiveScroll from "locomotive-scroll";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
+import { gsap, ScrollTrigger, ScrollSmoother } from "gsap-trial/all";
 import { ProjectsBlock, HomeBlock } from "../components/Blocks";
 
 import { Link, graphql } from "gatsby";
@@ -12,50 +10,24 @@ export default function IndexPage({ data: { projects, site } }) {
   const scrollRef = React.createRef();
 
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    const scroller = new locomotiveScroll({
-      el: scrollRef.current,
-      smooth: true,
-    });
-    scroller.on("scroll", ScrollTrigger.update);
-    ScrollTrigger.scrollerProxy(scrollRef.current, {
-      scrollTop(value) {
-        return arguments.length ? scroller.scrollTo(value, 0, 0) : scroller.scroll.instance.scroll.y;
+    let pinWrap = document.querySelector("#projectsWrapper");
+    let horizontalScrollLength = pinWrap.offsetWidth - window.innerWidth;
+    // Pinning and horizontal scrolling
+    gsap.to("#projectsWrapper", {
+      scrollTrigger: {
+        scrub: true,
+        trigger: "#blockProjects",
+        pin: true,
+        start: "top top",
+        end: () => "+=" + horizontalScrollLength,
       },
-      getBoundingClientRect() {
-        return {
-          left: 0,
-          top: 0,
-          width: window.innerWidth,
-          height: window.innerHeight,
-        };
-      },
-      pinType: scrollRef.current.style.transform ? "transform" : "fixed",
+      x: -horizontalScrollLength,
+      ease: "none",
     });
-    window.addEventListener("load", function () {
-      let pinWrap = document.querySelector("#projectsWrapper");
-      let pinWrapWidth = pinWrap.offsetWidth;
-      let horizontalScrollLength = pinWrapWidth - window.innerWidth;
-      // Pinning and horizontal scrolling
-      gsap.to("#projectsWrapper", {
-        scrollTrigger: {
-          scroller: scrollRef.current, //locomotive-scroll
-          scrub: true,
-          trigger: "#blockProjects",
-          pin: true,
-          // anticipatePin: 1,
-          start: "top top",
-          end: pinWrapWidth,
-        },
-        x: -horizontalScrollLength,
-        ease: "none",
-      });
-      ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
-      ScrollTrigger.refresh();
-    });
+    ScrollTrigger.refresh();
   });
   return (
-    <Layout scrollRef={scrollRef}>
+    <Layout>
       <HomeBlock />
       <ProjectsBlock projects={projects.nodes} />
     </Layout>
