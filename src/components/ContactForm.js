e024ad44c2a8c51561a729f673447861e5527c48
Michał Niewitała <niewitala@lightweb.pl>
2022-10-11 10:40:53 +0200

    Form file

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,35 +1,34 @@
 import React, { useState } from 'react'
 import * as styles from '../styles/contact-form.module.sass'
 
-function TextInput({ type = 'text', name }) {
-  const [value, setValue] = useState('')
+function TextInput({ type = 'text', name, required }) {
+  const [v, setV] = useState('')
   var label = name.charAt(0).toUpperCase() + name.slice(1)
 
   const handleInput = e => {
-    setValue(e.target.value)
+    setV(e.target.value)
   }
 
   return (
-    <p className={styles.field + (value && ' active')}>
+    <p className={styles.field + (v && ' active')}>
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
-      {type === 'textarea' ? (
-        <textarea className={styles.textarea} cols="50" rows="4" name={name} id={name} onChange={handleInput}></textarea>
-      ) : (
-        <input className={styles.textInput} type={type} name={name} id={name} onChange={handleInput} required />
-      )}
+      {type === 'textarea' && <textarea className={styles.textarea} cols="50" rows="4" name={name} id={name} onChange={handleInput}></textarea>}
+      {type !== 'textarea' && <input className={styles.textInput} type={type} name={name} id={name} onChange={handleInput} required={required} />}
     </p>
   )
 }
 
 export default function ContactForm({ id }) {
   return (
-    <form className={styles.contactForm} name="contact" method="post" data-netlify="true" onSubmit="submit">
+    <form className={styles.contactForm} name="contact" enctype="multipart/form-data" data-netlify="true" onSubmit="submit">
       <input type="hidden" name="form-name" value="contact" />
       <TextInput name="name" />
-      <TextInput name="email" type="email" />
-      <TextInput name="message" type="textarea" />
+      <TextInput name="email" type="email" required />
+      <TextInput name="message" type="textarea" required />
+      <TextInput name="file" type="file" required />
+
       <p className={styles.field}>
         <button className={styles.button} type="submit">
           Send message
