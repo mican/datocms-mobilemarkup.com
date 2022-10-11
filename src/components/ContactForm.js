95425a50b5104b03d27c9fbe4015bcfc437596ec
Michał Niewitała <niewitala@lightweb.pl>
2022-10-11 10:40:53 +0200

    Form file

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,39 +1,39 @@
-import React, { useState } from "react";
-import * as styles from "../styles/contact-form.module.sass";
+import React, { useState } from 'react'
+import * as styles from '../styles/contact-form.module.sass'
 
-function TextInput({ type = "text", name }) {
-  const [value, setValue] = useState("");
-  var label = name.charAt(0).toUpperCase() + name.slice(1);
+function TextInput({ type = 'text', name, required }) {
+  const [value, setValue] = useState('')
+  var label = name.charAt(0).toUpperCase() + name.slice(1)
 
-  const handleInput = (e) => {
-    setValue(e.target.value);
-  };
+  const handleInput = e => {
+    setValue(e.target.value)
+  }
 
   return (
-    <p className={styles.field + (value && " active")}>
+    <p className={styles.field + (value && ' active')}>
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
-      {type === "textarea" && <textarea className={styles.textarea} cols="50" rows="4" name={name} id={name} onChange={handleInput}></textarea>}
-      {type !== "textarea" && <input className={styles.textInput} type={type} name={name} id={name} onChange={handleInput} required />}
+      {type === 'textarea' && <textarea className={styles.textarea} cols="50" rows="4" name={name} id={name} onChange={handleInput}></textarea>}
+      {type !== 'textarea' && <input className={styles.textInput} type={type} name={name} id={name} onChange={handleInput} required={required} />}
     </p>
-  );
+  )
 }
 
-const ContactForm = ({ id }) => {
+export default function ContactForm({ id }) {
   return (
-    <form className={styles.contactForm} name="contact" method="post" data-netlify="true" onSubmit="submit">
+    <form className={styles.contactForm} name="contact" enctype="multipart/form-data" data-netlify="true" onSubmit="submit">
       <input type="hidden" name="form-name" value="contact" />
       <TextInput name="name" />
-      <TextInput name="email" type="email" />
-      <TextInput name="message" type="textarea" />
+      <TextInput name="email" type="email" required />
+      <TextInput name="message" type="textarea" required />
+      <TextInput name="file" type="file" required />
+
       <p className={styles.field}>
         <button className={styles.button} type="submit">
           Send message
         </button>
       </p>
     </form>
-  );
-};
-
-export default ContactForm;
+  )
+}
