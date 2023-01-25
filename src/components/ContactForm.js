2512a09ff780ea00d29934fc2e4f6bd1ebb4ba95
Michał Niewitała <niewitala@lightweb.pl>
2023-01-25 10:41:38 +0100

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
-import { NetlifyForm, Honeypot } from 'react-netlify-forms'
+import { NetlifyForm, Honeypot, useNetlifyFormContext } from 'react-netlify-forms'
 import * as styles from '../styles/contact-form.module.sass'
 
-function TextInput({ type = 'text', name, handleChange }) {
+function TextInput({ type = 'text', name }) {
   var label = name.charAt(0).toUpperCase() + name.slice(1)
+  const [value, setValue] = useState('')
+  const { handleChange } = useNetlifyFormContext()
 
   const handleInput = e => {
+    const { value } = e.target
+    setValue(value)
     handleChange(e)
   }
 
   return (
-    <p className={styles.field + ' active'}>
+    <p className={styles.field + (value && ' active')}>
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
@@ -31,9 +35,9 @@ export default function ContactForm() {
           <Honeypot />
           {success && <p>Thanks for contacting us!</p>}
           {error && <p>Sorry, we could not reach our servers. Please try again later.</p>}
-          <TextInput name="name" handleChange={handleChange} />
-          <TextInput name="email" type="email" handleChange={handleChange} />
-          <TextInput name="message" type="textarea" handleChange={handleChange} />
+          <TextInput name="name" />
+          <TextInput name="email" type="email" />
+          <TextInput name="message" type="textarea" />
           <p className={styles.field}>
             <button className={styles.button} type="submit">
               Send message
