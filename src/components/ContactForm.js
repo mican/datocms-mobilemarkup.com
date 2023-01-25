e1d9a9a08ba6f018a5add1a3377c0092acfda80b
Michał Niewitała <niewitala@lightweb.pl>
2023-01-25 09:59:22 +0100

    Fix hanleChange

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,21 +3,19 @@ import { NetlifyForm, Honeypot } from 'react-netlify-forms'
 import * as styles from '../styles/contact-form.module.sass'
 
 function TextInput({ type = 'text', name, handleChange }) {
-  const [value, setValue] = useState('')
   var label = name.charAt(0).toUpperCase() + name.slice(1)
 
   const handleInput = e => {
-    handleChange()
-    setValue(e.target.value)
+    handleChange(e)
   }
 
   return (
-    <p className={styles.field + (value && ' active')}>
+    <p className={styles.field + ' active'}>
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
       {type === 'textarea' ? (
-        <textarea className={styles.textarea} cols="50" rows="4" name={name} id={name} onChange={handleInput}></textarea>
+        <textarea className={styles.textarea} cols="50" rows="4" type="text" name={name} id={name} onChange={handleInput}></textarea>
       ) : (
         <input className={styles.textInput} type={type} name={name} id={name} onChange={handleInput} required />
       )}
