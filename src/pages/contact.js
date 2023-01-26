a532009f159733597a79a0ff9844a7886534871c
Michał Niewitała <niewitala@lightweb.pl>
2023-01-26 09:34:03 +0100

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,11 @@
-import * as React from "react";
+import * as React from 'react'
 
-import Layout from "../components/layout";
-import ContactForm from "../components/ContactForm.js";
+import Layout from '../components/layout'
+import ContactForm from '../components/ContactForm'
+import ServiceForm from '../components/ServiceForm'
+import * as styles from '../styles/page-contact.module.sass'
 
-import * as styles from "../styles/page-contact.module.sass";
-
-const ContactPage = () => {
+export default function ContactPage() {
   return (
     <Layout>
       <section className={styles.sectionContact}>
@@ -14,7 +14,5 @@ const ContactPage = () => {
         </div>
       </section>
     </Layout>
-  );
-};
-
-export default ContactPage;
+  )
+}
